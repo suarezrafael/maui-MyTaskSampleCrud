aa6e00dd89f57d91b7a81c5a2735d7637fef1ab0
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-04-17 19:51:57 -0300

    main view model

---
+++ MyTaskSampleCrud/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MyTaskSampleCrud.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();    
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+
+            // add our item
+            Items.Add(Text);
+            Text = string.Empty;
+        }
+      
+    }
+}
